04032bbaf4b91099b1bc18cf3f33f6b75fdbb855
王权 <n0rk1n@live.cn>
2018-08-22 13:18:12 +0800

    GsonFormat 插件测试

---
+++ oriki-commons/src/test/java/cn/oriki/commons/GsonFormatTest.java
@@ -0,0 +1,34 @@
+package cn.oriki.commons;
+
+/**
+ * JsonFormat 插件测试
+ *
+ * @author oriki
+ */
+public class GsonFormatTest {
+
+    /**
+     * name : zhangsan
+     * age : 18
+     */
+
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+}
