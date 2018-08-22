97228d4bea4fda507c3b91fdaa5b88bdf4a05771
王权 <n0rk1n@live.cn>
2018-08-22 14:45:54 +0800

    Reflects 添加 getField 和 getPublicMethods 方法

--- oriki-commons/src/main/java/cn/oriki/commons/util/Reflects.java
+++ oriki-commons/src/main/java/cn/oriki/commons/util/Reflects.java
@@ -3,8 +3,11 @@ package cn.oriki.commons.util;
 import com.google.common.collect.Lists;
 import lombok.NonNull;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 反射工具类
@@ -30,13 +33,62 @@ public class Reflects {
     }
 
     /**
-     * 获取字节码文件的所有 public method（包含父类）
+     * 获取字节码文件的所有 public method（包含父类 & Object）
+     * <p>
+     * 没有公开方法会返回空集合
      *
      * @param clazz 字节码文件
      * @return Class 对象中所有权限为 public 方法的对象
      */
     public static List<Method> getPublicMethods(@NonNull Class<?> clazz) {
         return Lists.newArrayList(clazz.getMethods());
+    }
+
+    /**
+     * 获取字节码文件的所有 public 方法，并剔除 Object 提供的公开方法
+     *
+     * @param clazz 字节码文件
+     * @return 剔除了 Object 中的公开方法的其余方法
+     */
+    public static List<Method> getPublicMethodsWithoutObject(@NonNull Class<?> clazz) {
+        List<Method> publicMethods = getPublicMethods(clazz);
+        List<Method> objectMethod = getPublicMethods(Object.class);
+        publicMethods.removeAll(objectMethod);
+        return publicMethods;
+    }
+
+    /**
+     * 获取字节码文件的所有 field (包含父类)
+     * <p>
+     * 没有属性会返回空集合
+     *
+     * @param clazz 字节码文件
+     * @return 对象中所有成员变量
+     */
+    public static List<Field> getFields(@NonNull Class<?> clazz) {
+        // 递归获取父类 Field
+        return getFields(Lists.newArrayList(), clazz);
+    }
+
+    /**
+     * 递归，获取 Class 本身及其父类中成员变量
+     *
+     * @param list  存储集合
+     * @param clazz Class 对象
+     * @param <T>   泛型
+     * @return 存储集合
+     */
+    private static <T> List<Field> getFields(@NonNull List<Field> list, Class<T> clazz) {
+        Field[] fields = clazz.getDeclaredFields();
+        // 存入本字节码文件所有 Field
+        if (fields != null && fields.length > 0) {
+            list.addAll(Arrays.asList(fields));
+        }
+        Class<? super T> superclass = clazz.getSuperclass();
+        if (Objects.nonNull(superclass) && !Object.class.equals(superclass)) {
+            getFields(list, superclass);
+        }
+        return list;
     }
 
 }
--- oriki-commons/src/test/java/cn/oriki/commons/util/ReflectsTest.java
+++ oriki-commons/src/test/java/cn/oriki/commons/util/ReflectsTest.java
@@ -3,8 +3,10 @@ package cn.oriki.commons.util;
 import cn.oriki.commons.entity.Children;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
@@ -20,11 +22,24 @@ public class ReflectsTest {
     public void getPublicMethods() {
         List<Method> publicMethods = Reflects.getPublicMethods(Children.class);
         System.out.println(publicMethods.size());
+
         List<Method> publicMethods1 = Reflects.getPublicMethods(Object.class);
         System.out.println(publicMethods1.size());
 
         // 共有 4 个属性，8个公共方法。其中 Object 中提供了 9 个公共方法
         assertEquals(publicMethods.size(), publicMethods1.size() + 8);
+    }
+
+    @Test
+    public void getPublicMethodsWithoutObject() {
+        List<Method> publicMethodsWithoutObject = Reflects.getPublicMethodsWithoutObject(Children.class);
+        assertEquals(Integer.valueOf(8), Integer.valueOf(publicMethodsWithoutObject.size()));
+    }
+
+    @Test
+    public void getFields() {
+        List<Field> fields = Reflects.getFields(Children.class);
+        assertEquals(Integer.valueOf(4), Integer.valueOf(fields.size()));
     }
 
 }
