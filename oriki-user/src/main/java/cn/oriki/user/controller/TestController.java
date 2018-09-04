41d31f71f31734ffc5007ce8ece1baf8ac1bcf87
SH-AD\oriki.wang <oriki.wang@chinascope.com>
2018-09-04 17:58:24 +0800

    检查某个服务的所有节点是否全部工作

--- oriki-user/src/main/java/cn/oriki/user/controller/TestController.java
+++ oriki-user/src/main/java/cn/oriki/user/controller/TestController.java
@@ -1,8 +1,11 @@
 package cn.oriki.user.controller;
 
+import cn.oriki.commons.response.Responses;
+import com.netflix.appinfo.InstanceInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +31,19 @@ public class TestController {
         // 获取 服务端注册的微服务
         List<String> serviceNames = this.discoveryClient.getServices();
         return serviceNames.toString();
+    }
+
+    @GetMapping(value = "/query-service-state")
+    public String queryServiceStateByServiceId(String serviceId) {
+        try {
+            this.discoveryClient.getInstances(serviceId).forEach(e -> {
+                InstanceInfo.InstanceStatus status = ((EurekaDiscoveryClient.EurekaServiceInstance) e).getInstanceInfo().getStatus();
+                assert InstanceInfo.InstanceStatus.UP.equals(status);
+            });
+            return Responses.responseSuccess("success");
+        } catch (Exception e) {
+            return Responses.responseFail("fail");
+        }
     }
 
     /**
